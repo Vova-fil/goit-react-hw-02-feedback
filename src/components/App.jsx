4fcde8815e9617553b74a84d5494dd8c45d3c997
Vova-fil <vfil.zp@gmail.com>
2022-05-02 00:04:10 +0300

    пофиксил проптайпы

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,20 +58,4 @@ export class App extends React.Component {
   }
 }
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         textTransform: 'uppercase',
-//         color: '#010101',
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
+
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -11,6 +11,6 @@ export function Section({ title, children }) {
 }
 
 Section.propTypes = {
-  title: PropTypes.string,
+  title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
